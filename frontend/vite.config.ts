0b06122bc864d5caf27e24dcba92538965436c49
Thomas Jay Rush <jrush@quickblocks.io>
2024-01-22 17:50:13 -0500

    Adds alias for commonly used paths for frontend code

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components/index"),
+      "@gocode": path.resolve(__dirname, "./wailsjs/go")
+    }
+  }
+});

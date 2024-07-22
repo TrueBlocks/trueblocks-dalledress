b774a9d54dd3f9d7d44bb4947c56df05dfe8dc89
Thomas Jay Rush <jrush@quickblocks.io>
2024-07-22 07:10:50 -0400

    Make names columns consistent

--- frontend/src/views/Names/NameTable.tsx
+++ frontend/src/views/Names/NameTable.tsx
@@ -1,49 +1,62 @@
 import React from "react";
 import { IconCircleCheck } from "@tabler/icons-react";
 import { types } from "@gocode/models";
-import { createColumnHelper } from "@tanstack/react-table";
+import { createColumnHelper, ColumnDef } from "@tanstack/react-table";
+import { CustomMeta } from "../CustomMeta";
+
+type CustomColumnDef<TData, TValue> = ColumnDef<TData, TValue> & {
+  meta?: CustomMeta;
+};
 
 const nameColumnHelper = createColumnHelper<types.Name>();
 
-export const nameColumns = [
+export const nameColumns: CustomColumnDef<types.Name, any>[] = [
   nameColumnHelper.accessor("address", {
     header: () => "Address",
-    cell: (info) => info.getValue(),
+    cell: (info) => info.renderValue(),
     size: 100,
+    meta: { className: "wide" },
   }),
   nameColumnHelper.accessor("tags", {
     header: () => "Tags",
     cell: (info) => info.renderValue(),
     size: 100,
+    meta: { className: "wide" },
   }),
   nameColumnHelper.accessor("name", {
     header: () => "Name",
     cell: (info) => info.renderValue(),
     size: 100,
+    meta: { className: "wide" },
   }),
   nameColumnHelper.accessor("symbol", {
     header: () => "Symbol",
     cell: (info) => info.renderValue(),
     size: 100,
+    meta: { className: "small" },
   }),
   nameColumnHelper.accessor("decimals", {
     header: () => "Decimals",
     cell: (info) => info.renderValue(),
     size: 100,
+    meta: { className: "small" },
   }),
   nameColumnHelper.accessor("isContract", {
     header: () => "isContract",
     cell: (info) => (info.getValue() ? <IconCircleCheck size={20} color="white" fill="green" /> : ""),
     size: 100,
+    meta: { className: "small-centered" },
   }),
   nameColumnHelper.accessor("isErc20", {
     header: () => "isErc20",
     cell: (info) => (info.getValue() ? <IconCircleCheck size={20} color="white" fill="green" /> : ""),
     size: 100,
+    meta: { className: "small-centered" },
   }),
   nameColumnHelper.accessor("isErc721", {
     header: () => "isErc721",
     cell: (info) => (info.getValue() ? <IconCircleCheck size={20} color="white" fill="green" /> : ""),
     size: 100,
+    meta: { className: "small-centered" },
   }),
 ];

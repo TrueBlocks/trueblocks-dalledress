4a98fd04cd9e47be0773b79ebbbc073f5b2683cb
Thomas Jay Rush <jrush@quickblocks.io>
2024-07-22 06:25:02 -0400

    Uses CustomMeta in NameView component

--- frontend/src/views/Names/NamesView.tsx
+++ frontend/src/views/Names/NamesView.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import classes from "@/App.module.css";
+import lClasses from "../Columns.module.css";
 import { GetNames, GetNamesCnt } from "@gocode/app/App";
 import { types } from "@gocode/models";
 import { flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
 import { Stack, Table, Title } from "@mantine/core";
 import { nameColumns } from "./NameTable";
+import { CustomMeta } from "../CustomMeta";
 import { View, ViewStatus } from "@components";
 import { useKeyboardPaging } from "@hooks";
 
@@ -27,7 +29,7 @@ export function NamesView() {
             {table.getHeaderGroups().map((headerGroup) => (
               <Table.Tr key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
-                  <Table.Th key={header.id}>
+                  <Table.Th key={header.id} className={lClasses.centered}>
                     {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
                   </Table.Th>
                 ))}
@@ -37,11 +39,14 @@ export function NamesView() {
           <Table.Tbody>
             {table.getRowModel().rows.map((row) => (
               <Table.Tr key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <Table.Td style={{ verticalAlign: "top" }} key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </Table.Td>
-                ))}
+                {row.getVisibleCells().map((cell) => {
+                  var meta = cell.column.columnDef.meta as CustomMeta;
+                  return (
+                    <Table.Td key={cell.id} className={lClasses[meta?.className || ""]}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </Table.Td>
+                  );
+                })}
               </Table.Tr>
             ))}
           </Table.Tbody>

892d0a1527a5f12f9ca35067204ed8b12a5b0e79
Thomas Jay Rush <jrush@quickblocks.io>
2024-07-22 22:49:33 -0400

    Keeps track of last used Route and Menu

--- frontend/src/components/global/Routes.tsx
+++ frontend/src/components/global/Routes.tsx
@@ -1,22 +1,34 @@
-import React from "react";
-import { Route, Switch } from "wouter";
+import React, { useEffect } from "react";
+import { Route, Switch, useLocation } from "wouter";
 import classes from "@/App.module.css";
-import HomeView from "@/views/Home/HomeView";
-import SettingsView from "@/views/Settings/SettingsView";
+import { HomeView, NamesView, SettingsView } from "@views";
+import { GetLast } from "@gocode/app/App";
 
-function Routes() {
+export const Routes = () => {
+  const [, setLocation] = useLocation();
+
+  useEffect(() => {
+    const lastRoute = (GetLast("route") || "/").then((route) => {
+      setLocation(route);
+    });
+  }, [setLocation]);
+
+  var menuItems = [
+    { route: "/names", component: NamesView },
+    { route: "/settings", component: SettingsView },
+    { route: "/", component: HomeView },
+  ];
+
   return (
     <div className={classes.mainContent}>
       <Switch>
-        <Route path="/settings">
-          <SettingsView />
-        </Route>
-        <Route>
-          <HomeView />
-        </Route>
+        {menuItems.map((item) => (
+          <Route key={item.route} path={item.route}>
+            <item.component />
+          </Route>
+        ))}
       </Switch>
     </div>
   );
-}
+};
 
-export default Routes;

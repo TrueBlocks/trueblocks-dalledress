3be295eba909eae01212c20611c65ebed6c826a5
Thomas Jay Rush <jrush@quickblocks.io>
2024-07-22 22:48:08 -0400

    Better backend

--- app/app.go
+++ app/app.go
@@ -9,15 +9,35 @@ import (
 )
 
 type App struct {
-	ctx     context.Context
-	session config.Session
+	ctx        context.Context
+	session    config.Session
+	apiKeys    map[string]string
+	namesMap   map[base.Address]NameEx
+	names      []NameEx // We keep both for performance reasons
+	ensMap     map[string]base.Address
+	renderCtxs map[base.Address][]*output.RenderCtx
+	// Add your application's data here
 }
 
 func NewApp() *App {
-	var a App
+	a := App{
+		apiKeys:    make(map[string]string),
+		namesMap:   make(map[base.Address]NameEx),
+		renderCtxs: make(map[base.Address][]*output.RenderCtx),
+		ensMap:     make(map[string]base.Address),
+		// Initialize maps here
+	}
 
 	// it's okay if it's not found
 	_ = a.session.Load()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	// } else if a.apiKeys["openAi"] = os.Getenv("OPENAI_API_KEY"); a.apiKeys["openAi"] == "" {
+	// 	log.Fatal("No OPENAI_API_KEY key found")
+	}
+
+	// Initialize your data here
 
 	return &a
 }
@@ -29,15 +49,26 @@ func (a App) String() string {
 
 func (a *App) Startup(ctx context.Context) {
 	a.ctx = ctx
+	if err := a.loadNames(); err != nil {
+		logger.Panic(err)
+	}
 }
 
 func (a *App) DomReady(ctx context.Context) {
+	// Sometimes useful for debugging
+	if os.Getenv("TB_CMD_LINE") == "true" {
+		return
+	}
 	runtime.WindowSetPosition(a.ctx, a.session.X, a.session.Y)
 	runtime.WindowSetSize(a.ctx, a.session.Width, a.session.Height)
 	runtime.WindowShow(a.ctx)
 }
 
 func (a *App) Shutdown(ctx context.Context) {
+	// Sometimes useful for debugging
+	if os.Getenv("TB_CMD_LINE") == "true" {
+		return
+	}
 	a.session.X, a.session.Y = runtime.WindowGetPosition(a.ctx)
 	a.session.Width, a.session.Height = runtime.WindowGetSize(a.ctx)
 	a.session.Y += 38 // TODO: This is a hack to account for the menu bar - not sure why it's needed

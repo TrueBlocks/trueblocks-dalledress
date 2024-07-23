8cc9cf76a5e7ea5fb20c6a6faf8b1b3a35316434
Thomas Jay Rush <jrush@quickblocks.io>
2024-07-22 22:45:12 -0400

    Enables rudimentary messaging from backend

--- frontend/src/components/view/ViewStatus.tsx
+++ frontend/src/components/view/ViewStatus.tsx
@@ -1,6 +1,50 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState, useEffect } from "react";
+import classes from "./ViewStatus.module.css";
+import { EventsOn, EventsOff } from "@runtime";
 import { Text } from "@mantine/core";
+import { MessageType } from "@gocode/app/App";
 
-export function ViewStatus({ children }: { children: ReactNode }) {
-  return <Text size="xs">{children}</Text>;
+// TODO: Why is this no availabe in the Wails folders?
+type Progress = {
+  address: string;
+  have: number;
+  want: number;
+};
+
+export function ViewStatus() {
+  const [statusMessage, setStatusMessage] = useState<string>("");
+  const [color, setColor] = useState<string>(classes.green);
+
+  useEffect(() => {
+    const handleDone = () => {
+      setStatusMessage("done");
+      setColor(classes.green);
+    };
+
+    const handleProgress = (p: Progress) => {
+      setStatusMessage(`Progress (${p.address}): ${p.have}/${p.want}`);
+      setColor(classes.green);
+    };
+
+    const handleError = (errorStr: string) => {
+      setStatusMessage(`Error: ${errorStr}`);
+      setColor(classes.red);
+    };
+
+    EventsOn("Completed", handleDone);
+    EventsOn("Progress", handleProgress);
+    EventsOn("Error", handleError);
+
+    return () => {
+      EventsOff("Done");
+      EventsOff("Progress");
+      EventsOff("Error");
+    };
+  }, []);
+
+  return (
+    <Text size="lg">
+      <div className={color}>{statusMessage}</div>
+    </Text>
+  );
 }

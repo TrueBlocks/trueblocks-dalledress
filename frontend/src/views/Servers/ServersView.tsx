917ccfcf4a4440050e946cc01a1d1c823c60e993
Thomas Jay Rush <jrush@quickblocks.io>
2024-07-25 08:01:07 -0400

    Adds a ServerCard component

--- frontend/src/views/Servers/ServersView.tsx
+++ frontend/src/views/Servers/ServersView.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { View, ViewStatus } from "@components";
+import { ServerCard } from "./ServerCard";
 import { servers } from "@gocode/models";
 import { GetServer /* , ToggleServer */ } from "@gocode/app/App";
 import { Stack, Title, SimpleGrid } from "@mantine/core";
@@ -19,12 +20,15 @@ export function ServersView() {
     updateServer("scraper", setScraper);
   }, []);
 
+  const toggleServer = (name: string) => {
+  };
+
   return (
     <View>
       <Title order={3}>Servers View</Title>
       <Stack className={classes.mainContent}>
         <SimpleGrid cols={2} spacing="lg" style={{ padding: "lg" }}>
-          <div>{JSON.stringify(scraper)}</div>
+          <ServerCard s={scraper} toggle={toggleServer} />
         </SimpleGrid>
       </Stack>
       <ViewStatus />

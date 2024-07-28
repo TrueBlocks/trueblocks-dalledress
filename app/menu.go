16bdc8c395a813004f55c8113cb5303f02b07b32
Thomas Jay Rush <jrush@quickblocks.io>
2024-07-28 10:25:25 -0400

    Cleaning

--- app/menu.go
+++ app/menu.go
@@ -7,21 +7,25 @@ import (
 
 func (a *App) GetMenus() *menu.Menu {
 	appMenu := menu.NewMenu()
+
 	systemMenu := appMenu.AddSubmenu("DalleDress")
 	systemMenu.AddText("About", nil, a.SystemAbout)
 	systemMenu.AddText("Quit", keys.CmdOrCtrl("q"), a.SystemQuit)
+
 	fileMenu := appMenu.AddSubmenu("File")
 	fileMenu.AddText("New File", keys.CmdOrCtrl("n"), a.FileNew)
 	fileMenu.AddText("Open File", keys.CmdOrCtrl("o"), a.FileOpen)
 	fileMenu.AddText("Save File", keys.CmdOrCtrl("s"), a.FileSave)
 	fileMenu.AddText("Save As File", keys.CmdOrCtrl("a"), a.FileSaveAs)
+
 	viewMenu := appMenu.AddSubmenu("View")
-	viewMenu.AddText("View 1", keys.CmdOrCtrl("1"), a.ViewHome)
-	viewMenu.AddText("View 2", keys.CmdOrCtrl("2"), a.ViewDalle)
-	viewMenu.AddText("View 3", keys.CmdOrCtrl("3"), a.ViewSeries)
-	viewMenu.AddText("View 4", keys.CmdOrCtrl("4"), a.ViewHistory)
-	viewMenu.AddText("View 5", keys.CmdOrCtrl("5"), a.ViewNames)
-	viewMenu.AddText("View 6", keys.CmdOrCtrl("6"), a.ViewServers)
-	viewMenu.AddText("View 7", keys.CmdOrCtrl("7"), a.ViewSettings)
+	viewMenu.AddText("Home", keys.CmdOrCtrl("1"), a.ViewHome)
+	viewMenu.AddText("Dalle", keys.CmdOrCtrl("2"), a.ViewDalle)
+	viewMenu.AddText("Series", keys.CmdOrCtrl("3"), a.ViewSeries)
+	viewMenu.AddText("History", keys.CmdOrCtrl("4"), a.ViewHistory)
+	viewMenu.AddText("Names", keys.CmdOrCtrl("5"), a.ViewNames)
+	viewMenu.AddText("Servers", keys.CmdOrCtrl("6"), a.ViewServers)
+	viewMenu.AddText("Settings", keys.CmdOrCtrl("7"), a.ViewSettings)
+
 	return appMenu
 }

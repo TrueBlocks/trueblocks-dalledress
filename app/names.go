c31542b5c1e8c30311ef11ac1bd1b01937bdc736
Thomas Jay Rush <jrush@quickblocks.io>
2025-05-03 21:09:47 -0400

    Updates

--- app/names.go
+++ app/names.go
@@ -1,14 +1,7 @@
 package app
 
 import (
-	"fmt"
-	"path/filepath"
-	"sync"
-
 	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/base"
-	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/config"
-	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/file"
-	"github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/names"
 	coreTypes "github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/types"
 	// "github.com/TrueBlocks/trueblocks-dalledress/pkg/types"
 )
@@ -25,53 +18,53 @@ func (a *App) GetNamesCnt() int {
 	return len(a.Names)
 }
 
-func (a *App) loadNames(wg *sync.WaitGroup) error {
-	defer func() {
-		if wg != nil {
-			wg.Done()
-		}
-	}()
+// func (a *App) loadNames(wg *sync.WaitGroup) error {
+// 	defer func() {
+// 		if wg != nil {
+// 			wg.Done()
+// 		}
+// 	}()
 
-	chain := "mainnet"
-	filePath := filepath.Join(config.MustGetPathToChainConfig(chain), string(names.DatabaseCustom))
-	lineCount, _ := file.WordCount(filePath, true)
-	customCount := 0
-	for _, name := range a.Names {
-		if name.Parts&coreTypes.Custom != 0 {
-			customCount++
-		} else {
-			break
-		}
-	}
-	if lineCount == customCount {
-		return nil
-	}
-	names.ClearCustomNames()
+// 	chain := "mainnet"
+// 	filePath := filepath.Join(config.MustGetPathToChainConfig(chain), string(names.DatabaseCustom))
+// 	lineCount, _ := file.WordCount(filePath, true)
+// 	customCount := 0
+// 	for _, name := range a.Names {
+// 		if name.Parts&coreTypes.Custom != 0 {
+// 			customCount++
+// 		} else {
+// 			break
+// 		}
+// 	}
+// 	if lineCount == customCount {
+// 		return nil
+// 	}
+// 	names.ClearCustomNames()
 
-	parts := coreTypes.Regular | coreTypes.Custom | coreTypes.Prefund | coreTypes.Baddress
-	if namesMap, err := names.LoadNamesMap(chain, parts, nil); err != nil {
-		return err
-	} else if (namesMap == nil) || (len(namesMap) == 0) {
-		return fmt.Errorf("no names found")
-	} else {
-		if len(a.Names) == len(namesMap) {
-			return nil
-		}
+// 	parts := coreTypes.Regular | coreTypes.Custom | coreTypes.Prefund | coreTypes.Baddress
+// 	if namesMap, err := names.LoadNamesMap(chain, parts, nil); err != nil {
+// 		return err
+// 	} else if (namesMap == nil) || (len(namesMap) == 0) {
+// 		return fmt.Errorf("no names found")
+// 	} else {
+// 		if len(a.Names) == len(namesMap) {
+// 			return nil
+// 		}
 
-		a.Names = namesMap // names = types.SummaryName{
-		// 	NamesMap: namesMap,
-		// 	Names:    []coreTypes.Name{},
-		// }
-		// for _, name := range a.names.NamesMap {
-		// 	a.names.Names = append(a.names.Names, name)
-		// }
-		// sort.Slice(a.names.Names, func(i, j int) bool {
-		// 	return compare(a.names.Names[i], a.names.Names[j])
-		// })
-		// a.names.Summarize()
-		return nil
-	}
-}
+// 		a.Names = namesMap // names = types.SummaryName{
+// 		// 	NamesMap: namesMap,
+// 		// 	Names:    []coreTypes.Name{},
+// 		// }
+// 		// for _, name := range a.names.NamesMap {
+// 		// 	a.names.Names = append(a.names.Names, name)
+// 		// }
+// 		// sort.Slice(a.names.Names, func(i, j int) bool {
+// 		// 	return compare(a.names.Names[i], a.names.Names[j])
+// 		// })
+// 		// a.names.Summarize()
+// 		return nil
+// 	}
+// }
 
 // func compare(nameI, nameJ coreTypes.Name) bool {
 // 	ti := nameI.Parts
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.23.1
 
 require (
 	github.com/TrueBlocks/trueblocks-core/src/apps/chifra v0.0.0-20250422130252-4a2fc83a6800
-	github.com/TrueBlocks/trueblocks-dalle/v2 v2.0.0-20250503124524-70135a6e7f2f
+	github.com/TrueBlocks/trueblocks-dalle/v2 v2.0.0-20250503130226-41a6e34513e4
 	github.com/TrueBlocks/trueblocks-sdk/v5 v5.2.0
 	github.com/fsnotify/fsnotify v1.9.0
 	github.com/google/go-cmp v0.7.0

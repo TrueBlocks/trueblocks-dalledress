e3181fc0bb1ef52c3516a8be08e144c2432b24c0
Thomas Jay Rush <jrush@quickblocks.io>
2025-07-12 06:14:02 -0400

    Cleanings

--- pkg/types/data_facet.go
+++ pkg/types/data_facet.go
@@ -1,15 +1,30 @@
 package types
 
-import "strings"
+import (
+	"strings"
+	"sync"
+)
 
 type DataFacet string
 
-var AllDataFacets = []struct {
-	Value  DataFacet `json:"value"`
-	TSName string    `json:"tsname"`
-}{}
+var (
+	AllDataFacets = []struct {
+		Value  DataFacet `json:"value"`
+		TSName string    `json:"tsname"`
+	}{}
+	dataFacetMap = map[DataFacet]bool{}
+	dfMutex      sync.Mutex
+)
 
 func RegisterDataFacet(df DataFacet) {
+	dfMutex.Lock()
+	defer dfMutex.Unlock()
+
+	if _, exists := dataFacetMap[df]; exists {
+		return
+	}
+	dataFacetMap[df] = true
+
 	AllDataFacets = append(AllDataFacets, struct {
 		Value  DataFacet `json:"value"`
 		TSName string    `json:"tsname"`

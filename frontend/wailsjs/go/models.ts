f2c6f2ad9159c76bb95dbee03f301a5baefe46d0
Thomas Jay Rush <jrush@quickblocks.io>
2025-08-22 19:19:49 -0400

    Cleanings

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -892,6 +892,14 @@ export namespace status {
 
 export namespace types {
 	
+	export enum LoadState {
+	    STALE = "stale",
+	    FETCHING = "fetching",
+	    PARTIAL = "partial",
+	    LOADED = "loaded",
+	    PENDING = "pending",
+	    ERROR = "error",
+	}
 	export enum DataFacet {
 	    DOWNLOADED = "downloaded",
 	    KNOWN = "known",
@@ -925,14 +933,6 @@ export namespace types {
 	    STATUS = "status",
 	    CACHES = "caches",
 	    CHAINS = "chains",
-	}
-	export enum LoadState {
-	    STALE = "stale",
-	    FETCHING = "fetching",
-	    PARTIAL = "partial",
-	    LOADED = "loaded",
-	    PENDING = "pending",
-	    ERROR = "error",
 	}
 	export class Parameter {
 	    components?: Parameter[];

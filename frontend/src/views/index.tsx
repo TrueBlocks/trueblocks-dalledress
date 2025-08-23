7266c410a7dfbc9c369715d56cfe0a6b9d1ae9b3
Thomas Jay Rush <jrush@quickblocks.io>
2025-08-23 06:32:00 -0400

    Preparing for dalledress

--- frontend/src/views/index.tsx
+++ frontend/src/views/index.tsx
@@ -5,9 +5,6 @@
  * This file was auto generated. Do not edit.
  */
 
-export { Projects } from './projects';
-export { DalleDress } from './DalleDress';
-
 export { Exports } from './exports';
 export { Monitors } from './monitors';
 export { Abis } from './abis';
@@ -15,7 +12,10 @@ export { Names } from './names';
 export { Chunks } from './chunks';
 export { Contracts } from './contracts';
 export { Status } from './status';
+export { DalleDress } from './dalledress';
+
 export { Khedra } from './khedra';
+export { Projects } from './projects';
 
 export * from './settings';
 export * from './utils';
